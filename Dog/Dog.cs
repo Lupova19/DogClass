573bfbe8be2e8edfd42d4748b9e33db755436f44
Lupova19 <nadiqlupova@gmail.com>
2024-09-22 17:48:39 +0300

    first commit

---
+++ Dog/Dog.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dog
+{
+    public class Dog
+    {
+        private string breed;
+        private string name;
+        private int age;
+        private string ownerName;
+
+        public string Breed
+        {
+            get { return breed; }
+            set { breed = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string OwnerName
+        {
+            get { return ownerName; }
+            set { ownerName = value; }
+        }
+
+        public Dog(string breed, string name, int age, string ownerName)
+        {
+            this.Breed = breed;
+            this.Name = name;
+            this.Age = age;
+            this.OwnerName = ownerName;
+        }
+
+        public override string ToString()
+        {
+            return $"Bred: {this.Breed}, Name:{this.Name}, Age: {this.Age}, Owner: {this.OwnerName}";
+        }
+    }
+}
---
+++ Dog/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dog
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dog> dogs = new List<Dog>();
+            Console.Write("Enter start(or another word) to continue: ");
+            string input = Console.ReadLine();
+
+            while (input != "stop")
+            {
+                Console.WriteLine("Enter breed:");
+                string breed = Console.ReadLine();
+
+                Console.WriteLine("Enter name:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter age:");
+                int age = int.Parse(Console.ReadLine());
+
+                if (age > 30)
+                {
+                    Console.WriteLine("You have a mistake!");
+                }
+
+                Console.WriteLine("Enter owner:");
+                string ownerName = Console.ReadLine();
+
+                Dog dog = new Dog(breed, name, age, ownerName);
+                dogs.Add(dog);
+
+                Console.Write("Enter start(or other word) if you want to continue, otherwise enter stop: ");
+                input = Console.ReadLine(); 
+            }
+
+            foreach (var dog in dogs)
+            {
+                Console.WriteLine(dog);
+            }
+
+        }
+    }
+}
